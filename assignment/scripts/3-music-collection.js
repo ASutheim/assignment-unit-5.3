05fc5c848129ac795540e097dc23084cc46a41a2
Anna Sutheim <asutheim@gmail.com>
2023-04-28 20:51:12 -0500

    Added more albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,21 +3,32 @@ console.log("***** Music Collection *****");
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-  let newestAlbum = { title, artist, yearPublished };
-  collection.push(newestAlbum);
-  return newestAlbum;
+  let album = { title, artist, yearPublished };
+  collection.push(album);
+  return album;
 }
 
 /* The code above creates an object 'newAlbum' with the properties of title, artist, and year published, 
 and adds this object to the end of the array 'collection */
 
 addToCollection("Candide", "Leonard Bernstein", "1956");
+console.log(addToCollection("Candide", "Leonard Bernstein", "1956"));
+
 addToCollection("A Night at the Opera", "Queen", "1975");
+console.log(addToCollection("A Night at the Opera", "Queen", "1975"));
+
 addToCollection("Coconut Oil", "Lizzo", "2016");
+console.log(addToCollection("Coconut Oil", "Lizzo", "2016"));
+
 addToCollection("A Badly Broken Code", "Dessa", "2010");
+console.log(addToCollection("A Badly Broken Code", "Dessa", "2010"));
+
 addToCollection("Le Fil", "Camille", "2005");
+console.log(addToCollection("Le Fil", "Camille", "2005"));
+
 addToCollection("Cabaret Songs", "Malena Ernman", "2001");
+console.log(addToCollection("Cabaret Songs", "Malena Ernman", "2001"));
 
 console.log(collection);
 
-/*the code above tests the addToCollection function*/
+/*the code above tests the addToCollection function by adding six albums*/
