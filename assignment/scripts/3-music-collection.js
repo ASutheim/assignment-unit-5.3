6b024ad9ffcb15ef55310979069ba9e0c14530c8
Anna Sutheim <asutheim@gmail.com>
2023-04-28 21:07:32 -0500

    created and tested showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,24 +11,33 @@ function addToCollection(title, artist, yearPublished) {
 /* The code above creates an object 'newAlbum' with the properties of title, artist, and year published, 
 and adds this object to the end of the array 'collection */
 
-addToCollection("Candide", "Leonard Bernstein", "1956");
 console.log(addToCollection("Candide", "Leonard Bernstein", "1956"));
 
-addToCollection("A Night at the Opera", "Queen", "1975");
 console.log(addToCollection("A Night at the Opera", "Queen", "1975"));
 
-addToCollection("Coconut Oil", "Lizzo", "2016");
 console.log(addToCollection("Coconut Oil", "Lizzo", "2016"));
 
-addToCollection("A Badly Broken Code", "Dessa", "2010");
 console.log(addToCollection("A Badly Broken Code", "Dessa", "2010"));
 
-addToCollection("Le Fil", "Camille", "2005");
 console.log(addToCollection("Le Fil", "Camille", "2005"));
 
-addToCollection("Cabaret Songs", "Malena Ernman", "2001");
 console.log(addToCollection("Cabaret Songs", "Malena Ernman", "2001"));
 
 console.log(collection);
 
 /*the code above tests the addToCollection function by adding six albums*/
+
+function showCollection(array) {
+  console.log(array.length);
+  for (i = 0; i < array.length; i++) {
+    console.log(
+      array[i].title,
+      "by",
+      array[i].artist,
+      "published in",
+      array[i].yearPublished
+    );
+  }
+}
+
+showCollection(collection);
